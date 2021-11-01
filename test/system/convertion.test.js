54ec0afe0f4036024260d51c78fceeed4aca3c7c
Carlos_Veloz <carlos.veloz@wizeline.com>
2021-11-01 09:43:04 -0600

    working on script to test options

--- test/system/convertion.test.js
+++ test/system/convertion.test.js
@@ -1,27 +1,24 @@
 const expect = require('chai').expect,
-    {
-        SchemaPack
-    } = require('../../lib/SchemaPack'),
+    Index = require('../../index.js'),
     fs = require('fs'),
     outputDirectory = 'test/data/output/';
 
 describe('E2E flows', function () {
     it('Should conver a har file into a PM Collection', function () {
-        let fileContent = fs.readFileSync('test/data/externalHARfile/patio.wizeline.givers.har', 'utf8');
-        const schemaPack = new SchemaPack({
-            data: fileContent,
-            type: 'string'
-        }, {});
-
-        schemaPack.convert((error, result) => {
-            expect(error).to.be.null;
-            expect(result).to.be.an('object');
-            expect(result.output).to.be.an('array');
-            expect(result.output[0].data).to.be.an('object');
-            expect(result.output[0].type).to.equal('collection');
-            fs.writeFileSync(outputDirectory + 'collection.json',
-                JSON.stringify(result.output[0].data));
-        });
+        let fileContent = fs.readFileSync('test/data/externalHARfile/patio.wizeline.receivers.har', 'utf8');
+        Index.convert(
+            { data: fileContent, type: 'string' },
+            { indentCharacter: 'tab' },
+            ( error, result) => {
+                expect(error).to.be.null;
+                expect(result).to.be.an('object');
+                expect(result.output).to.be.an('array');
+                expect(result.output[0].data).to.be.an('object');
+                expect(result.output[0].type).to.equal('collection');
+                fs.writeFileSync(outputDirectory + 'collection.json',
+                    JSON.stringify(result.output[0].data));
+            }
+        );
     
     });
 });
